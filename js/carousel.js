c824e977f74a48fc3bd8424924c227655ddb632f
Raymond0817 <ramon.d.santos0817@gmail.com>
2025-05-30 11:06:25 -0300

    adicionando

--- js/carousel.js
+++ js/carousel.js
@@ -8,11 +8,16 @@ let carouselArr = [];
 
 //class Carousel
 class Carousel {
-
+constructor(imagem, texto, link){
+    this.imagem = imagem;
+    this.texto = texto;
+    this.link = link;
+}
     
-      
+     
     static Start(arr){
         if(arr){
+            console.log(carouselArr);
 
             if(arr.length > 0){
                 Carousel._sequence = 0;
@@ -27,6 +32,7 @@ class Carousel {
     }
 
     static Next(){
-        
+        document.getElementById("carousel-title").innerHTML = carouselArr[2].texto
+        document.getElementById("image").src = carouselArr[2].imagem
     }
 };
